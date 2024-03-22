9b70519d040acaf42e7cf2d70c263b9b230befb6
Tim Spears <timspears@imarc.com>
2024-03-22 12:13:19 -0400

    fix export

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import babel from 'vite-plugin-babel'
 
 export default defineConfig(async ({ command }) => {
   const config = {
-    build: {
-      manifest: true,
-      outDir: 'web/dist',
-      assetsDir: '.',
-      rollupOptions: {
-        input: [
-          'index.js'
-        ]
-      },
-      target: 'esnext'
-    },
     plugins: [
       babel({
         babelConfig: {
